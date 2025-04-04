be5d0d8cde2e950bbe55d9a204050e45f7c29e2a
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-04-03 22:59:10 -0600

    Implement functions in VentanaInicio controller to handle Play, About, Exit buttons and play background music

--- src/main/java/com/mycompany/primerproyectobatallanaval/VentanaInicioController.java
+++ src/main/java/com/mycompany/primerproyectobatallanaval/VentanaInicioController.java
@@ -1,28 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package com.mycompany.primerproyectobatallanaval;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author randa
- */
 public class VentanaInicioController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     * @param url
-     * @param rb
-     */
+    @FXML
+    private Button ButtonJugar;
+    @FXML
+    private Button ButtonAcercaDe;
+    @FXML
+    private Button ButtonSalir;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        Reproductor.getInstance(getClass().getResource("/Musica/Song15.mp3").toExternalForm(), 1).PlayOnBucle();
+    }
+
+    @FXML
+    private void OnJugarPrecionado(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("VentanaNiveles.fxml"));
+            Stage stage = (Stage) ButtonJugar.getScene().getWindow();
+            stage.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void OnAcercaDePrecionado(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Acerca de");
+        alert.setHeaderText("Información del Juego: \n* Nombre del Curso: Programación II.\n* Estudiante: Randall Anel Arauz Cubilla.\n* Ciclo: III.\n* Año: 2025.");
+        alert.setContentText("========== INSTRUCCIONES ==========\n"
+                + "1. Posiciona tus barcos en el tablero propio.\n"
+                + "2. Ataca al tablero enemigo seleccionando casillas.\n"
+                + "3. Gana el juego hundiendo todos los barcos enemigos antes de que el rival hunda los tuyos.\n"
+                + "================================");
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void OnSalirPrecionado(ActionEvent event) {
+        Reproductor.getInstance(null, 0).Stop(); // Detiene la música
+        Platform.exit(); // Cierra la aplicación
+    }
 }
