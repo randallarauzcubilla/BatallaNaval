0bb75041bb75f640d14022cbbbb57eb24ed96e0e
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-04-03 23:42:02 -0600

    Add UsuarioData class to store and retrieve user name

---
+++ src/main/java/com/mycompany/primerproyectobatallanaval/UsuarioData.java
@@ -0,0 +1,14 @@
+package com.mycompany.primerproyectobatallanaval;
+
+public class UsuarioData {
+
+    private static String nombreUsuario;
+
+    public static String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public static void setNombreUsuario(String nombre) {
+        nombreUsuario = nombre;
+    }
+}
