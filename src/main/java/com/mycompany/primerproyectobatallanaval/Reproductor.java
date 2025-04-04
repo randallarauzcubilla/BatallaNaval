2129bc985faa9100977e1a870d35ac50265db96f
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-04-03 22:19:42 -0600

    implement Reproductor class with functionalities for playing, pausing, stopping, and managing audio

--- src/main/java/com/mycompany/primerproyectobatallanaval/Reproductor.java
+++ src/main/java/com/mycompany/primerproyectobatallanaval/Reproductor.java
@@ -1,9 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.mycompany.primerproyectobatallanaval;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 /**
  *
@@ -11,4 +9,52 @@ package com.mycompany.primerproyectobatallanaval;
  */
 public class Reproductor {
 
+    private static Reproductor instance;
+    private static MediaPlayer mediaPlayer;
+
+    // Constructor privado para evitar instancias directas
+    private Reproductor(String audioPath, int type) {
+        if (mediaPlayer == null) {
+            Media media = new Media(audioPath);
+            mediaPlayer = new MediaPlayer(media);
+            if (type == 1) {
+                mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+            }
+        }
+    }
+
+    // Método estático para obtener la única instancia
+    public static Reproductor getInstance(String audioPath, int type) {
+        if (instance == null) {
+            instance = new Reproductor(audioPath, type);
+        }
+        return instance;
+    }
+
+    public void PlayOnBucle() {
+        if (mediaPlayer != null && mediaPlayer.getStatus() != MediaPlayer.Status.PLAYING) {
+            mediaPlayer.play();
+        }
+    }
+
+    public void Stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.dispose();
+            mediaPlayer = null;
+            instance = null; // Resetear la instancia si se detiene
+        }
+    }
+
+    public void Pause() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
+    }
+
+    public void setVolumen(double volumen) {
+        if (mediaPlayer != null) {
+            mediaPlayer.setVolume(volumen);
+        }
+    }
 }
