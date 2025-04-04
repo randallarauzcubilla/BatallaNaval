1304f93295207d8792895f4b5c4c567fee369fba
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-04-03 22:20:38 -0600

    add javafx.media module requirement to support audio playback

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.mycompany.primerproyectobatallanaval {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
-
+    requires javafx.media;
     opens com.mycompany.primerproyectobatallanaval to javafx.fxml;
     exports com.mycompany.primerproyectobatallanaval;
 }
