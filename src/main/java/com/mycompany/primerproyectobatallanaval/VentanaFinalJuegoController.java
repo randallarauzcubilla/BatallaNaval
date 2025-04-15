1213584afcb92a0a993fcba4aef6de74fa1e9873
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-04-14 21:36:00 -0600

    Implemented new background music for each game state

--- src/main/java/com/mycompany/primerproyectobatallanaval/VentanaFinalJuegoController.java
+++ src/main/java/com/mycompany/primerproyectobatallanaval/VentanaFinalJuegoController.java
@@ -55,14 +55,17 @@ public class VentanaFinalJuegoController implements Initializable {
             case "ganador":
                 mensajeFinalLabel.setText("¡Ganaste!");
                 imagenResultado.setImage(new Image(getClass().getResourceAsStream(rutaBase + "victoria.jpg")));
+                Reproductor.getInstance(getClass().getResource("/Musica/IntroVictoria.mp3").toExternalForm(), 0).PlayUnaVez();
                 break;
             case "perdedor":
                 mensajeFinalLabel.setText("¡Perdiste!");
                 imagenResultado.setImage(new Image(getClass().getResourceAsStream(rutaBase + "Derrota.jpg")));
+                Reproductor.getInstance(getClass().getResource("/Musica/IntroDerrota.mp3").toExternalForm(), 0).PlayUnaVez();
                 break;
             case "empate":
                 mensajeFinalLabel.setText("¡Empataste!");
                 imagenResultado.setImage(new Image(getClass().getResourceAsStream(rutaBase + "empatee.jpg")));
+                Reproductor.getInstance(getClass().getResource("/Musica/IntroEmpate.mp3").toExternalForm(), 0).PlayUnaVez();
                 break;
         }
     }
@@ -70,6 +73,7 @@ public class VentanaFinalJuegoController implements Initializable {
     @FXML
     private void volverAVentanaInicio(ActionEvent event) {
         try {
+            Reproductor.detenerMusicaSiActiva();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("VentanaInicio.fxml"));
             Parent root = loader.load();
             Stage stage = (Stage) btnVolverInicioDesdeFinJuego.getScene().getWindow();
@@ -85,6 +89,7 @@ public class VentanaFinalJuegoController implements Initializable {
     @FXML
     private void volverAVentanaJuego(ActionEvent event) {
         try {
+            Reproductor.detenerMusicaSiActiva();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("VentanaNiveles.fxml"));
             Parent root = loader.load();
             Stage stage = (Stage) btnVolverJugar.getScene().getWindow();
